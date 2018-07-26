ffc601d5754042a57174383a10989c419f4a8dd1
cosmotherly <cosmotherly@atlassian.com>
2018-07-26 12:41:25 +1000

    fix: audiences would be wrapped in extra quotes

--- asap-cli/src/init.rs
+++ asap-cli/src/init.rs
@@ -79,6 +79,6 @@ fn parse_audience(input: String) -> Aud {
     if input.contains(",") {
         Aud::from_vec(input.split(',').collect())
     } else {
-        Aud::from_str(format!("\"{}\"", input))
+        Aud::from_str(format!("{}", input))
     }
 }
